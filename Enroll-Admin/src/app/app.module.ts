8e8fc09963b52fff3de85e40aaa608b1dc0b8167
Mikołaj Kozieł <mkoziel045@gmail.com>
2020-12-05 19:09:20 +0100

    Components init

--- Enroll-Admin/src/app/app.module.ts
+++ Enroll-Admin/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AdminPanelComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Enroll-Admin/src/app/login/login.component.ts
+++ Enroll-Admin/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, of } from 'rxjs';
+// import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  mail: any;
+  password: any;
+  user = null;
 
-  constructor() { }
+  userEmitter = new BehaviorSubject(this.user);   
+
+  constructor(
+    // private authService: AuthService,
+    // private wycieczkiService: WycieczkiServiceService,
+    // private dbService: DbService
+    ) { }
 
   ngOnInit(): void {
+    // this.getUser();
+  }
+
+  getUser(): void{
+    // this.authService.getUserObs()
+    // .subscribe((x: any)=> {
+    //   console.log(x);
+    //   this.assignAndEmmitUser(x);
+    // });
+  }
+
+  assignAndEmmitUser(x:any){
+    // if(x){
+    //   this.user = x.mail;
+    // }else{
+    //   this.user = null;
+    // }
+    // this.userEmitter.next(this.user);
+  }
+
+  login(){
+    // this.authService.login(this.mail, this.password)
+  }
+
+  logout(){
+    // this.authService.logout()
   }
 
 }
