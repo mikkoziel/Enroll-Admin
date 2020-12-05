879c0d8cae1f36bc676105508723f1a810c34c9b
Mikołaj Kozieł <mkoziel045@gmail.com>
2020-12-05 20:12:13 +0100

    Added login and admin component

--- Enroll-Admin/src/app/app-routing.module.ts
+++ Enroll-Admin/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: './login', pathMatch: 'full'},
-  { path: './login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminPanelComponent },
 ];
 
 @NgModule({
